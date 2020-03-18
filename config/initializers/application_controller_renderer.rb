4f66ecd3ae634ca55b5b087bc140c337e4fcd12e
oe0315 <oeoe0315@gmail.com>
2020-03-18 18:40:13 +0900

    5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd248cf1c40ebfec2de42db60bfb4d56710f5ad5300753c3c2e46cc476ec3eced202bf330b92aabe63c9134c8b8e4953433bdc3e4a950113c575611f0155f28ae'
